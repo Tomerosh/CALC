481be6cfc807369528d6ca1e4c0530e231e1c315
tomerosh <tomerosh@gmail.com>
2026-06-11 20:15:17 +0300

    Handle pull request conflicts

--- draft4.py
+++
@@ -1,4 +0,0 @@
-import sympy
-
-x, y = sympy.symbols('x y')
-print( 2*x**2 ** 3*y**4  )
--- frontend/client.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-exp = input('Enter expression: ')
-response = requests.post(f'http://10.0.0.17:8000/solve', params={'expression': exp})
-print(response.json())
